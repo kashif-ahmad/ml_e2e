b51d20a059d85a28ae1e268e2d5c36a1a8ede89c
Ahmad <touchbasewith@protonmail.com>
2024-09-14 12:03:58 -0400

    model training complete

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,16 +1,17 @@
 import sys
-import os
 from dataclasses import dataclass
 
-import numpy as np
+import numpy as np 
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
-from src.logger import logging
 from src.exception import CustomException
+from src.logger import logging
+import os
+
 from src.utils import save_object
 
 @dataclass
