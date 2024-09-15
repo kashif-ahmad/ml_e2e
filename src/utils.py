21070c22c42b73cf831a81dc2ffd3baa0484ff09
Ahmad <touchbasewith@protonmail.com>
2024-09-15 10:41:40 -0400

    Hyperparameter Tuning applied

--- src/utils.py
+++ src/utils.py
@@ -11,6 +11,8 @@ from sklearn.model_selection import GridSearchCV
 from src.logger import logging
 from src.exception import CustomException
 
+from sklearn.model_selection import GridSearchCV
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -30,7 +32,15 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+            #model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
 
